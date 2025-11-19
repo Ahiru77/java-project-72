be4dec2b6f0c09d17cfb2c0db22583f80c5f2f3a
Ahiru77 <ev.limatov@gmail.com>
2025-11-19 15:51:16 +0300

    Do project init

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+    id("application")
+    id("org.sonarqube") version "7.0.1.6134"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+ application {
+     mainClass = "hexlet.code.App"
+ }
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("io.javalin:javalin:5.6.2")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Ahiru77_java-project-72")
+        property("sonar.organization", "ahiru77")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
